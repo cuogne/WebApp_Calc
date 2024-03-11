0416715149a60126afa1a72cbd022ddb4f419c18
Cường <143596996+cuogne@users.noreply.github.com>
2024-03-12 01:00:25 +0700

    func

--- calc.py
+++ calc.py
@@ -1,11 +1,40 @@
-def add (a,b):
-    return a + b
+import math
 
-def subtract (a,b):
-    return a - b
+def add(x, y):
+    return x + y
 
-def multiply (a,b):
-    return a * b
+def subtract(x, y):
+    return x - y
 
-def divide (a,b):
-    return a / b 
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        st.error("Cannot divide by zero.")
+        return "ERROR"
+    return x / y
+
+def mod (x, y):
+    if y == 0:
+        st.error("Cannot divide by zero.")
+        return "ERROR"
+    return x % y
+
+def factorial(x):
+    return math.factorial(int(x))
+
+def square_root(x):
+    if (x < 0):
+        st.error("Cannot calculator square root of Negative")
+        return "ERROR"
+    return math.sqrt(x)
+
+def exponentiation(x,y):
+    if x == 0 and y == 0 :
+        st.error("Cannot exponentiation 0 with 0")
+        return "ERROR"
+    elif y > 2000 :
+        st.error("Overflow Number")
+        return "ERROR"
+    return x**y
