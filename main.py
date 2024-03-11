8d7dd0d150a143d514036f49f93102c3030f9ae3
Cường <143596996+cuogne@users.noreply.github.com>
2024-03-12 01:13:22 +0700

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
 import streamlit as st
-from calc import add, subtract, multiply, divide, mod, factorial, square_root, exponentiation
+import math
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        st.error("Cannot divide by zero.")
+        return "ERROR"
+    return x / y
+
+def mod (x, y):
+    if y == 0:
+        st.error("Cannot divide by zero.")
+        return "ERROR"
+    return x % y
+
+def factorial(x):
+    return math.factorial(int(x))
+
+def square_root(x):
+    if (x < 0):
+        st.error("Cannot calculator square root of Negative")
+        return "ERROR"
+    return math.sqrt(x)
+
+def exponentiation(x,y):
+    if x == 0 and y == 0 :
+        st.error("Cannot exponentiation 0 with 0")
+        return "ERROR"
+    elif y > 2000 :
+        st.error("Overflow Number")
+        return "ERROR"
+    return x**y 
+
 def main():
     st.title("Calculator")
+
+    # Hiển thị nút chọn cho các phép toán cơ bản
     operation = st.radio("Select operation:", ["Add", "Subtract", "Multiply", "Divide", "Mod","Factorial", "Square Root", "Exponentiation"])
+
     number1 = st.number_input("Enter the first number: ", value = 0)
     number2 = None
+
     if operation in ["Add", "Subtract", "Multiply", "Divide", "Mod", "Exponentiation"]:
         number2 = st.number_input("Enter the second number: ", value = 0)
+
     if operation == "Factorial" or operation == "Square Root":
         number2 = None
 
@@ -33,3 +78,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# --- cuogne ----
