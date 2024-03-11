f72be2aa42fa721c49e2ccd489cdf8e2f9ae90f0
Cường <143596996+cuogne@users.noreply.github.com>
2024-03-12 01:03:22 +0700

    main.py

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 import streamlit as st
-from calc import add, subtract, multiply, divide
-
+from calc import Add, Subtract, Multiply, Divide, Mod, Factorial, Square Root, Exponentiation
 def main():
-    st.title("Web App Calculator by cuogne")
-    number1 = st.number_input("Enter the first number: ", value=0)
-    number2 = st.number_input("Enter the second number: ", value=0)
+    st.title("Calculator")
+    operation = st.radio("Select operation:", ["Add", "Subtract", "Multiply", "Divide", "Mod","Factorial", "Square Root", "Exponentiation"])
+    number1 = st.number_input("Enter the first number: ", value = 0)
+    number2 = None
+    if operation in ["Add", "Subtract", "Multiply", "Divide", "Mod", "Exponentiation"]:
+        number2 = st.number_input("Enter the second number: ", value = 0)
+    if operation == "Factorial" or operation == "Square Root":
+        number2 = None
 
-    col1, col2, col3, col4 = st.columns(4)
-
-    with col1:
-        if st.button("Add"):
+    if st.button("Calculate"):
+        result = 0
+        if operation == "Add":
             result = add(number1, number2)
-            st.write(f"{number1} + {number2} = {result}")
-            st.balloons()
-
-    with col2:
-        if st.button("Subtract"):
+        elif operation == "Subtract":
             result = subtract(number1, number2)
-            st.write(f"{number1} - {number2} = {result}")
-            st.balloons()
-
-    with col3:
-        if st.button("Multiply"):
+        elif operation == "Multiply":
             result = multiply(number1, number2)
-            st.write(f"{number1} x {number2} = {result}")
-            st.balloons()
-
-    with col4:
-        if st.button("Divide"):
-            if number2 == 0:
-                st.write("Division cannot be performed with a dividend of 0")
-                return
-
+        elif operation == "Divide":
             result = divide(number1, number2)
-            st.write(f"{number1} / {number2} = {result}")
-            st.balloons()
+        elif operation == "Mod":
+            result = mod(number1,number2)
+        elif operation == "Factorial":
+            result = factorial(number1)
+        elif operation == "Square Root":
+            result = square_root(number1)
+        elif operation == "Exponentiation":
+            result = exponentiation(number1, number2);
+
+        st.write(f"The result of {operation} is: {result}")
 
 if __name__ == "__main__":
     main()
